3d00da406b0222a3b9c3123a45536ed36ae75bd9
ramgaikwad24 <ramgaikwad24@gmail.com>
2022-10-14 13:15:52 +0530

    first commit

---
+++ src/main/java/com/tut/App.java
@@ -0,0 +1,42 @@
+package com.tut;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SessionFactory factory=new Configuration().configure().buildSessionFactory();
+        Student st=new Student();
+        st.setId(104);
+        st.setName("ganu");
+        st.setCity("nanded");
+        System.out.println(st);
+        
+        Employee emp=new Employee();
+        emp.setId(201);
+        emp.setName("Archana");
+        emp.setDesignation("Account");
+        System.out.println(emp);
+        
+        Session session=factory.openSession();
+        Transaction tx=session.beginTransaction();
+        session.save(st);
+        session.save(emp);
+        tx.commit();
+        session.close();
+        
+        
+       // System.out.println(factory);
+        //System.out.println(factory.isClosed());
+    }
+}
